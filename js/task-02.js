6574cee1395ecf6aa0227c072c54c94f0a50735e
Bohdana-Halenko <danaermolenko20@gmail.com>
2021-10-28 15:29:16 +0300

    task02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const arr = ingredients.map(ingredient => {
+  const li = document.createElement("li")
+  li.textContent = ingredient;
+  li.classList.add("item");
+  return li;
+}
+);
+const ul = document.querySelector("#ingredients");
+ul.append(...arr);
+console.log(ul);
